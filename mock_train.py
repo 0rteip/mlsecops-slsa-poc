920aa7f1e3160a135a747402014091bc53ed5b17
0rteip <94202558+0rteip@users.noreply.github.com>
2026-08-19 11:19:42 +0200

    Add mock training script for model and BOM generation

---
+++ mock_train.py
@@ -0,0 +1,8 @@
+print("Training Model")
+
+with open("yolo_trained_model.pt", "w") as y:
+  y.write("super well trained model")
+
+with open("mlbom.json", "w") as m:
+  m.write(' {"bomFormat": "CycloneDX","specVersion": "1.5"}')
+
